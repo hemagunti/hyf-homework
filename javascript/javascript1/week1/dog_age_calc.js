ee9127b94e11747b6977554efe6cc3e2f3249022
hemagunti <hema.gunti02@gmail.com>
2019-03-06 07:43:56 +0100

    updated dog age calculator file

--- javascript/javascript1/week1/dog_age_calc.js
+++ javascript/javascript1/week1/dog_age_calc.js
@@ -4,11 +4,15 @@ let dogYearFuture = 2045;
 let dogYear = dogYearFuture - dogYearOfBirth;
 let humanYear = dogYear * 7;
 /*Declaring variable with Boolean type*/
-let shouldShowResultInDogYears = false;
+let shouldShowResultInDogYears = true;
 
 /*checking if dogs age is in dog age or in humans age*/
- if(shouldShowResultInDogYears === true)
-    console.log("your dog will be" + " "+ dogYear +" "+ "years old in" + " "+ dogYearFuture);
+ if(shouldShowResultInDogYears)
+ {
+    console.log("your dog will be" + " "+ dogYear +" "+ "dog years old in" + " "+ dogYearFuture);
+ }
     else
-    console.log("your dog will be" + " "+ humanYear +" "+ "years old in" + " "+ dogYearFuture);
+    {
+    console.log("your dog will be" + " "+ humanYear +" "+ "human years old in" + " "+ dogYearFuture);
+    }
  
