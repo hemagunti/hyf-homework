f8a7eb6eee2db708ccdec9986a5ccaeecd290d32
hemagunti <hema.gunti02@gmail.com>
2019-09-11 11:00:54 +0200

    git hub users

---
+++ react/week4/githubusers/src/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Inputtext from "./inputText";
+import Userlist from "./userList";
+
+function getGithubUsers(user) {
+    console.log(user);
+    return fetch(`https://api.github.com/search/users?q=${user}`).then(res =>
+        res.json()
+    );
+}
+
+class App extends React.Component {
+    state = {
+        searchResult: [],
+        isSearching: false
+    };
+    filterBySearch = (searchValue, inSearchingMode) => {
+        /* const { searchResult } = this.state; */
+        this.setState({ isSearching: true });
+        console.log(searchValue);
+        getGithubUsers(searchValue).then(users => {
+            console.log(users.items);
+            this.setState({ searchResult: users.items, isSearching: false });
+        });
+    };
+    componentDidMount() {
+        alert(
+            "type name or part of username and click on search button to find user"
+        );
+    }
+
+    render() {
+        const { searchResult, isSearching } = this.state;
+        return (
+            <div>
+                <h1>Find GITHUB USERS</h1>
+                <Inputtext filterBySearch={this.filterBySearch} />
+                {isSearching ? (
+                    <p>Loading...</p>
+                ) : (
+                        <Userlist searchResult={searchResult} />
+                    )}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+/* serviceWorker.unregister();
+ */
---
+++ react/week4/githubusers/src/userList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Userlist extends React.Component {
+    render() {
+        const { searchResult } = this.props;
+        return (
+            <div>
+                {searchResult.length > 0 ? (
+                    <ul>
+                        {searchResult.map(user => (
+                            <li key={user.id}>
+                                <ul>
+                                    <li>{user.login}</li>
+                                    <li>
+                                        <a href={user.html_url}>{user.html_url}</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                        <p>No Items found</p>
+                    )}
+            </div>
+        );
+    }
+}
+export default Userlist;
